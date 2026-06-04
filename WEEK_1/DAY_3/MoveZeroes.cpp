b07772550143ab0efb096bc84fdb054a925930c1
Divyansh <divyanshmaurya10001@gmail.com>
2026-06-04 16:28:33 +0530

    Problem_1

--- WEEK_1/DAY_3/MoveZeroes.cpp
+++ WEEK_1/DAY_3/MoveZeroes.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;  // position for next non-zero element
-
+        int j = 0; 
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != 0) {
                 swap(nums[i], nums[j]);
