1f3208f8e516a920afcf96da164f78fb7393f422
Divyansh <divyanshmaurya10001@gmail.com>
2026-06-04 16:19:28 +0530

    Problem_1

---
+++ WEEK_1/DAY_1/TwoSum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp; // value -> index
+
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+
+        // check if complement exists
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
+        }
+
+        // store current value and index
+        mp[nums[i]] = i;
+    }
+
+    return {}; // no solution
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = twoSum(nums, target);
+
+    cout << "Indices: " << result[0] << " " << result[1] << endl;
+    return 0;
+}
